98a727615912497c309f275ff53952e30587d1e1
huangYiYuan <414771197@qq.com>
2018-07-15 10:57:12 +0800

    创建新环境

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
-/*给webpack判断环境，给开发环境用development，在正式环境用production*/
